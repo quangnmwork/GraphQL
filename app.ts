41147d9ce9cdf77395b79f7755138b13793d58d6
Quang Nguyen <quangnm.work@gmail.com>
2022-03-17 21:29:16 +0700

    add resolve rootQuery

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
 const express = require("express");
-const graphqlHTTP = require("express-graphql");
-
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./model/schema");
 const app = express();
 
-app.use("/graphql", graphqlHTTP({})); // handle graphqh at this route
+app.use("/graphql", graphqlHTTP({ schema: schema })); // handle graphqh at this route
 
 app.listen(8000, () => console.log("App running"));
--- model/schema.ts
+++ model/schema.ts
@@ -1,6 +1,13 @@
+const lodash = require("lodash");
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const books = [
+  { name: "The wind", genre: "Fantasy", id: "1" },
+  { name: "The wind", genre: "Fantasy", id: "2" },
+  { name: "The wind", genre: "Fantasy", id: "3" },
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -13,4 +20,25 @@ const BookType = new GraphQLObjectType({
       type: GraphQLString,
     },
   }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve(parent: any, args: { id: string }) {
+        // code to get data from db or other source
+        console.log(parent);
+        return lodash.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
