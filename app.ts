f80f634668e584f4c3a34bbe5f3a11220db98369
Quang Nguyen <quangnm.work@gmail.com>
2022-03-17 22:11:19 +0700

    add AuthorType

--- app.ts
+++ app.ts
@@ -3,6 +3,6 @@ const { graphqlHTTP } = require("express-graphql");
 const schema = require("./model/schema");
 const app = express();
 
-app.use("/graphql", graphqlHTTP({ schema: schema })); // handle graphqh at this route
+app.use("/graphql", graphqlHTTP({ schema: schema, graphiql: true })); // handle graphqh at this route
 
 app.listen(8000, () => console.log("App running"));
--- model/schema.ts
+++ model/schema.ts
@@ -1,23 +1,48 @@
 const lodash = require("lodash");
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = graphql;
 
 const books = [
-  { name: "The wind", genre: "Fantasy", id: "1" },
-  { name: "The wind", genre: "Fantasy", id: "2" },
-  { name: "The wind", genre: "Fantasy", id: "3" },
+  { name: "The wind", genre: "Fantasy", id: 1 },
+  { name: "The wind", genre: "Fantasy", id: 2 },
+  { name: "The wind", genre: "Fantasy", id: 3 },
+];
+
+const authors = [
+  { name: "The wind", age: 12, id: 1 },
+  { name: "The wind", age: 15, id: 2 },
+  { name: "The wind", age: 22, id: 3 },
 ];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: {
       type: GraphQLString,
     },
     genre: {
       type: GraphQLString,
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: {
+      type: GraphQLString,
+    },
+    age: {
+      type: GraphQLInt,
     },
   }),
 });
@@ -28,12 +53,19 @@ const RootQuery = new GraphQLObjectType({
     book: {
       type: BookType,
       args: {
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
       },
-      resolve(parent: any, args: { id: string }) {
+      resolve(parent: any, args: { id: { type: typeof GraphQLID } }) {
         // code to get data from db or other source
         console.log(parent);
         return lodash.find(books, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent: any, args: { id: { type: typeof GraphQLID } }) {
+        return lodash.find(authors, { id: args.id });
       },
     },
   },
